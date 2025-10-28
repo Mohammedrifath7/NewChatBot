7e28d79d0b44db4da28e133f32f6da2e80d5c181
bemebe <mohammedrifath.@email.com>
2025-10-28 21:10:48 +0530

    Push

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ st.markdown("""
 st.title("💬 Welcome to ChatBot!")
 
 # -------------------- ALLOWED USERS --------------------
-ALLOWED_USERS = {"rifath", "marzooka"}
+ALLOWED_USERS = {"rifath", "marzooka","swathi"}
 
 # -------------------- SESSION STATE --------------------
 if "username" not in st.session_state:
