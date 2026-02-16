7b3298fb205ca2fae4c7d6c77b218d61c5206475
Mohammedrifath7 <rifathjainulla12345@gmail.com>
2026-02-16 16:23:33 +0530

    New log added

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ st.markdown("""
 st.title("💬 Welcome to ChatBot!")
 
 # -------------------- ALLOWED USERS --------------------
-ALLOWED_USERS = {"rifath", "marzooka","swathi","adlin","karthika","harsh","sachi"}
+ALLOWED_USERS = {"rifath", "marzooka","swathi","adlin","karthika","harsh","sachi","sruthi"}
 
 # -------------------- SESSION STATE --------------------
 if "username" not in st.session_state:
